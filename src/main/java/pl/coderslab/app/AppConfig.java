c61bd534d121d98f048b45398845bdcc26950733
ElektrycznyPies <zen.zukowski@gmail.com>
2024-07-05 18:45:26 +0200

    - working -

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,97 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+public class AppConfig implements WebMvcConfigurer {
+
+
+}
+
+
+
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = "pl.coderslab")
+//@EnableJpaRepositories(basePackages = "pl.coderslab.repository")
+//@EnableTransactionManagement
+//public class AppConfig implements WebMvcConfigurer {
+//    @Bean
+//    public LocalEntityManagerFactoryBean entityManagerFactory() {
+//        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+//        entityManagerFactoryBean.setPersistenceUnitName("bookstorePersistenceUnit");
+//        return entityManagerFactoryBean;
+//    }
+//
+//    @Bean
+//    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+//        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory);
+//        return jpaTransactionManager;
+//    }
+//
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(responseBodyConverter());
+//    }
+//
+//    @Bean
+//    public HttpMessageConverter<String> responseBodyConverter() {
+//        StringHttpMessageConverter converter = new StringHttpMessageConverter();
+//        converter.setSupportedMediaTypes(List.of(new MediaType("text", "html", StandardCharsets.UTF_8)));
+//        return converter;
+//    }
+//
+////    @Bean
+////    public PublisherConverter publisherConverter() {
+////        return new PublisherConverter();
+////    }
+////
+////    @Bean
+////    public AuthorConverter authorConverter() {
+////        return new AuthorConverter();
+////    }
+////
+////    @Override
+////    public void addFormatters(FormatterRegistry registry) {
+////        registry.addConverter(publisherConverter());
+////        registry.addConverter(authorConverter());
+////    }
+//
+//    @Bean
+//    public Validator validator() {
+//        return new LocalValidatorFactoryBean();
+//    }
+//
+//    @Bean(name = "localeResolver")
+//    public LocaleContextResolver getLocaleContextResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//        localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+//        return localeResolver;
+//    }
+//}
---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,18 @@
+package pl.coderslab.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.domain.Book;
+
+@RestController // równoznaczna z ResponseBody
+@RequestMapping("/books")
+public class BookController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+}
